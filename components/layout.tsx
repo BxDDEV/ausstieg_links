2c9f76d6f1da17e750971fa8b42f63acce7169c1
BxD <benex_dev@protonmail.com>
2024-02-08 10:44:26 +0100

    new test for favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
       <>
       <Head>
-        <link rel="icon" href="favicon.ico"/>
+        <link rel="icon" href="icon.ico"/>
       </Head>
       <div>
         <Navbar/>
