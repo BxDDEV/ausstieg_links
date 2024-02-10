d06a37743f7ec38457e1e03982883db072260d0f
BxD <benex_dev@protonmail.com>
2024-02-10 10:48:39 +0100

    change on contactform

--- components/kontaktformular/index.tsx
+++ components/kontaktformular/index.tsx
@@ -1,50 +1,45 @@
 
 import { ValidationError, useForm } from '@formspree/react'
 import styles from './style.module.css'
+import Link from 'next/link'
 
-export default function Kontaktformular()
-{
+export default function Kontaktformular() {
   const [state, handleSubmit] = useForm("ss")
-if(state.succeeded) {
-  return <p>Thanks</p>
-}    
-    return (
-      <>
-      <div style={{textAlign:'center', paddingTop:'2%'}}>
+  if (state.succeeded) {
+    return <p>Danke</p>
+  }
+  return (
+    <>
+      <div style={{ textAlign: 'center', paddingTop: '2%' }}>
         <h1>Kontakt</h1>
       </div>
-
       <div className={styles.form}>
-      <form onSubmit={handleSubmit}>
-
-      <label htmlFor="email">
-        Email:
-      </label>
-      
-      <input  type='email' name='email'/>
-      <ValidationError prefix='Email' field='email' errors={state.errors}/>
-      <label htmlFor="name">
-        Name:
-      </label>
-      <input id='name' type='text' name='name'/>
-      <ValidationError prefix='Name' field='name' errors={state.errors}/>
-      <label htmlFor="message">
-        Message:
-      </label>
-      <textarea id='message' name='message'/>
-      <ValidationError prefix='Email' field='email' errors={state.errors}/>
-
-      
-
-      <button className={styles.submitButton} type="submit" disabled={state.submitting}>
-        Senden
-      </button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">
+            Email:
+          </label>
+          <input id='email' type='email' name='email' />
+          <ValidationError prefix='Email' field='email' errors={state.errors} />
+          <label htmlFor="name">
+            Name:
+          </label>
+          <input id='name' type='text' name='name' />
+          <ValidationError prefix='Name' field='name' errors={state.errors} />
+          <label htmlFor="message">
+            Nachricht:
+          </label>
+          <textarea id='message' name='message' />
+          <ValidationError prefix='Nachricht' field='message' errors={state.errors} />
+          <input type="checkbox" id='accept'/>
+          <ValidationError prefix='' field='accept' errors={state.errors} />
+          <label className={styles.datenschutzerklärung} htmlFor="accept"> Ich habe die <Link href="datenschutz" target='_blank' className={styles.datenschutz}>Datenschutzerklärung</Link> zur Kenntnis genommen.</label>
+          <br />
+          <br />
+          <button className={styles.submitButton} type="submit" disabled={state.submitting}>
+            Senden
+          </button>
+        </form>
       </div>
-
-
-
-
-      </>
-    )
+    </>
+  )
 }
